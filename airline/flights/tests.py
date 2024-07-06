dee946c19b82ca88ef8cc89c543294ac3475bf03
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2024-07-06 20:02:40 +0300

    github actions

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+from django.db.models import Max
+from django.test import Client, TestCase
 
+from .models import Airport, Flight, Passenger
 # Create your tests here.
+
+class FlightTestCase(TestCase):
+
+    def setUp(self):
+
+        # Creating Airports
+        port1 = Airport.objects.create(code="AAA", city="City A")
+        port2 = Airport.objects.create(code="BBB", city="City B")
+
+        # Create Flights
+        Flight.objects.create(origin=port1, destination=port2, duration=100)
+        Flight.objects.create(origin=port1, destination=port1, duration=200)
+        Flight.objects.create(origin=port1, destination=port2, duration=-100)
+
+    def test_departures_count(self):
+        port = Airport.objects.get(code="AAA")
+        self.assertEqual(port.departures.count(), 3)
+
+    def test_arrivals_count(self):
+        port = Airport.objects.get(code="AAA")
+        self.assertEqual(port.arrivals.count(), 1)
+
+    def test_valid_flight(self):
+        port1 = Airport.objects.get(code="AAA")
+        port2 = Airport.objects.get(code="BBB")
+        flight = Flight.objects.get(origin=port1, destination=port2, duration=100)
+        self.assertTrue(flight.is_valid_flight())
+    
+    def test_invalid_flight(self):
+        port1 = Airport.objects.get(code="AAA")
+        flight = Flight.objects.get(origin=port1, destination=port1)
+        self.assertFalse(flight.is_valid_flight())
+
+    def test_invalid_duration(self):
+        port1 = Airport.objects.get(code="AAA")
+        port2 = Airport.objects.get(code="BBB")
+        flight = Flight.objects.get(origin=port1, destination=port2, duration=-100)
+        self.assertFalse(flight.is_valid_flight())
+
+    def test_index(self):
+        dummyClient = Client()
+        response = dummyClient.get("/flights/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["flights"].count(), 3)
+
+    def test_valid_flight_page(self):
+        port1 = Airport.objects.get(code="AAA")
+        flight = Flight.objects.get(origin=port1, destination=port1)
+        
+        dummyClient = Client()
+        response = dummyClient.get(f"/flights/{flight.id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_flight_page(self):
+        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
+          
+        dummyClient = Client()
+        response = dummyClient.get(f"/flights/{max_id + 1}")
+        self.assertEqual(response.status_code, 404)
+
+    def test_flight_page_passengers(self):
+        flight = Flight.objects.get(pk=1)
+        passenger = Passenger.objects.create(first="Alice", last="Adams")
+        flight.passengers.add(passenger)
+    
+        dummyClient = Client()
+        response = dummyClient.get(f"/flights/{flight.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+    
+    def test_flight_page_non_passengers(self):
+        flight = Flight.objects.get(pk=1)
+        passenger = Passenger.objects.create(first="Alice", last="Adams")
+    
+        dummyClient = Client()
+        response = dummyClient.get(f"/flights/{flight.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["non_passengers"].count(), 1)
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from .models import Flight, Passenger
@@ -10,15 +10,18 @@ def index(request):
         "flights": Flight.objects.all(),
     })
 
-def flight(request, flight_id: int):
-    flight = Flight.objects.get(pk=flight_id)
-    return render(request, "flights/flight.html",{
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(id=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight not found.")
+    return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
-def book(request, flight_id):
+def book(request, flight_id: int):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
         passenger_id = int(request.POST["passenger"])
